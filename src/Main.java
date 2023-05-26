00a2855d9e8cb57902cf3dea50e1b4d39b144055
Shree Thakre <shreet365@gmail.com>
2023-05-26 15:29:43 +0530

    Countin frequency of elements

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,26 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args)
     {
-        int arr[]={50,60,70,80};
+        int arr[]={1,1,4,1,2,4,3,1,4,3};
+        int a=0,b=0,c=0,d=0;
         for(int i =0;i<arr.length;i++)
         {
-            System.out.println(arr[i]);
+            if(arr[i]==1)
+            {
+                a++;
+            } else if (arr[i]==2) {
+                b++;
+            }
+            else if (arr[i]==3) {
+                c++;
+            }
+            else if (arr[i]==4) {
+                d++;
+            }
         }
+        System.out.println("Frequency of 1 is : "+a);
+        System.out.println("Frequency of 2 is : "+b);
+        System.out.println("Frequency of 3 is : "+c);
+        System.out.println("Frequency of 4 is : "+d);
     }
 }
