12ec1a0d3ace186e22fa934d8e557eba332f8bf6
Shree Thakre <shreet365@gmail.com>
2023-05-26 16:25:20 +0530

    Finding Largest Number from Array

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,17 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args)
     {
-        int arr[]={1,1,4,1,2,4,3,1,4,3};
-        int a=0,b=0,c=0,d=0;
+        int arr[]={10,30,50,40,60,90,50};
+        int iMax= 0;
         for(int i =0;i<arr.length;i++)
         {
-            if(arr[i]==1)
+            if(arr[i]>iMax)
             {
-                a++;
-            } else if (arr[i]==2) {
-                b++;
+                iMax = arr[i];
             }
-            else if (arr[i]==3) {
-                c++;
-            }
-            else if (arr[i]==4) {
-                d++;
-            }
+
         }
-        System.out.println("Frequency of 1 is : "+a);
-        System.out.println("Frequency of 2 is : "+b);
-        System.out.println("Frequency of 3 is : "+c);
-        System.out.println("Frequency of 4 is : "+d);
+        System.out.println("Max number is: "+iMax);
+
     }
 }
