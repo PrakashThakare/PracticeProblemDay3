3683a14af94185420a33decef89795d257861b98
Shree Thakre <shreet365@gmail.com>
2023-05-26 16:43:28 +0530

    Minimum number in Array

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args)
     {
-        int arr[]={10,30,50,40,60,90,50};
-        int iMax= 0;
+        int arr[]={30,50,40,30,10,90,50,};
+        int iMin= 0;
         for(int i =0;i<arr.length;i++)
         {
-            if(arr[i]>iMax)
+            if(arr[i]<arr[3])
             {
-                iMax = arr[i];
+                iMin = arr[i];
             }
 
         }
-        System.out.println("Max number is: "+iMax);
+        System.out.println("Min number is: "+iMin);
 
     }
 }
