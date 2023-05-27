b8737c6433b0e9409dad64100e5ec0be27ebef2f
Shree Thakre <shreet365@gmail.com>
2023-05-27 17:29:13 +0530

    Odd pisition Number Preinting

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        int arr[]={20,39,10,7,8,91,98,33,55,22,74,};
-        int iMin= arr[0];
-       for(int i=arr.length-1;i>=0;i--)
-        {
-            System.out.println(arr[i]);
+    public static void main(String[] args) {
+        int arr[] = {20, 39, 10, 7, 8, 91, 98, 33, 55, 22, 74,};
+        for (int i = 1; i < arr.length; i = i + 2) {
+
+            System.out.println(" elements of an array present on odd position: " + arr[i]);
+
         }
     }
 }
