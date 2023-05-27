5dd07165947b43fc278a4df033d6b51a3fd32ddd
Shree Thakre <shreet365@gmail.com>
2023-05-27 18:06:11 +0530

    Duplicate element in Array

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {20, 39, 10, 7, 8, 91, 98, 33, 55, 22, 74,};
-        for (int i = 1; i < arr.length; i = i + 2) {
-
-            System.out.println(" elements of an array present on odd position: " + arr[i]);
-
+        int arr[] = {20, 39, 10, 7, 8, 20,7,55,10};
+        int i=0;
+        for( i = 0; i < arr.length; i++)
+        {
+            for(int j = i + 1; j < arr.length; j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    System.out.println(arr[j]);
+                }
+            }
         }
     }
 }
